08b3ea181601924b3e07538b4724192924b71f2f
kishu1474 <56560864+kishu1474@users.noreply.github.com>
2020-12-10 21:33:29 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,10 +1,8 @@
-package project_compilers;
-
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class Main {
+public class My_code_2 {
 
 	static String[][] variables = new String[10][2];
 	static int k = 0;
@@ -21,9 +19,9 @@ public class Main {
 			if (tokens == null || tokens.isEmpty()) {
 				break;
 			}
-			
-			if(!tokens.contains(";")) {
-				System.out.println("Without semicolon not allowed !!");
+
+			if (!tokens.contains(";")) {
+				System.out.println("without semicolon not allowed!");
 				break;
 			}
 			String[] lines = tokens.split(";", -2);
@@ -33,8 +31,7 @@ public class Main {
 
 				if (lines[i] == null || lines[i].isEmpty()) {
 					continue;
-				} 
-				else {
+				} else {
 					statement(lines[i]);
 				}
 			}
@@ -45,7 +42,7 @@ public class Main {
 	public static void statement(String lines) {
 
 		String[] s = lines.split("=", 2);
-		
+
 		if(s.length < 2) {
 			System.out.println("Uninitialized Variable !!");
 			System.exit(0);
@@ -56,55 +53,59 @@ public class Main {
 
 		String[] splitted = expression.split("[-+*()]");
 		for (int i = 0; i < splitted.length; i++) {
-				
-				for(int q = 0; q <= k-1; q++) {
+			
+			for (int q = 0; q <= k - 1; q++) {
+				int w = 1;
+				for (w = q + w; w <= k; w++) {
 					
-					int w = 1;
-					for(w = q+w; w <= k; w++) {
-						
-						if(splitted[i].equals(variables[q][0])) {
-							expression = expression.replace(splitted[i], variables[q][1]);
-						}
+					if (splitted[i].equals(variables[q][0])) {
+						expression = expression.replace(splitted[i], variables[q][1]);
 					}
 				}
+			}
 		}
-
+		
 		variables[k][0] = identifier;
 		variables[k][1] = expression;
-		
+
 		identifier = identifier(variables[k][0]);
-		
+
 		expression = expression(variables[k][1]);
-		
+
 		int c = 0;
-		for(int t=0; t<expression.length(); t++) {
-			if(expression.charAt(t) == '-') {
+		for (int t = 0; t < expression.length(); t++) {
+			if (expression.charAt(t) == '-') {
 				c++;
 			}
 		}
+
 		expression = expression.substring(c);
-		if(c % 2 == 1) {
+		if (c % 2 == 1) {
 			expression = "-" + expression;
 		}
-		
-		if(identifier.equals("")) {
+
+		if (expression.equals("-0")) {
+			expression = "0";
+		}
+	
+		if (identifier.equals("")) {
 			System.out.println("Invalid Identifier!");
-			System.exit(0); 
+			System.exit(0);
+		}
+		
+		if (expression.equals("")) {
+			System.out.println("Invalid Value!");
+			System.exit(0);
 		}
 
-		if(expression.equals("")) {
-			System.out.println("Invalid Value!");
-			System.exit(0); 
-		}
-		
 		String statement = identifier + " = " + expression;
 
 		System.out.println("Output Statement: " + statement);
-		
+
 		variables[k][0] = identifier;
 		variables[k][1] = expression;
-		
-		k++; 
+
+		k++;
 	}
 
 	public static String expression(String expression) {
@@ -112,18 +113,17 @@ public class Main {
 		String final_exp = "";
 		String final_term = "";
 		String final_expression_output = "";
-		String regex = "[+-]?[1-9]+";
+		String regex = "[+-]?[0-9]+";
 		int e = 0, t = 0, et_output = 0;
-	
-		  if (expression.contains("*")) {
-			  
-			  final_term += term(expression);
-			  final_expression_output += final_term;
-			  
-			  return final_expression_output; 
-		  }
-		 
-		
+
+		if (expression.contains("*")) {
+			
+			final_term += term(expression);
+			final_expression_output += final_term;
+			
+			return final_expression_output;
+		}
+
 		if (!expression.contains("+") && !expression.contains("-")) {
 			
 			final_term += term(expression);
@@ -132,34 +132,30 @@ public class Main {
 			return final_expression_output;
 		}
 
-		for(int l=0; l<expression.length(); l++) {
-			
+		for (int l = 0; l < expression.length(); l++) {
 			if (expression.charAt(l) == '(') {
 				
 				int j;
 				for (j = l + 1; j < expression.length(); j++) {
-					
 					if (expression.charAt(j) == ')') {
 						
 						int p;
 						for (p = j + 1; p < expression.length(); p++) {
-							
 							if (expression.charAt(p) == '+') {
 								
-								final_exp += expression(expression.substring(l, j+1));
-								final_term += expression(expression.substring(p+1));
+								final_exp += expression(expression.substring(l, j + 1));
+								final_term += expression(expression.substring(p + 1));
 								
-								final_expression_output += final_exp +"+"+final_term;
+								final_expression_output += final_exp + "+" + final_term;
 								final_expression_output = expression(final_expression_output);
 								
 								return final_expression_output;
-							}
-							else if (expression.charAt(p) == '-') {
+							} else if (expression.charAt(p) == '-') {
 								
-								final_exp += expression(expression.substring(l, j+1));
-								final_term += expression(expression.substring(p+1));
+								final_exp += expression(expression.substring(l, j + 1));
+								final_term += expression(expression.substring(p + 1));
 								
-								final_expression_output += final_exp +"-"+ final_term;
+								final_expression_output += final_exp + "-" + final_term;
 								final_expression_output = expression(final_expression_output);
 								
 								return final_expression_output;
@@ -172,99 +168,68 @@ public class Main {
 				l = j;
 				break;
 			}
-		}	
-		
+		}
+
 		for (int i = 0; i < expression.length(); i++) {
-			
-			if(expression.charAt(0) == '+' || expression.charAt(0) == '-'
-					|| expression.charAt(0) == '(' || expression.contains("*")) {
-					
+
+			if (expression.charAt(0) == '+' || expression.charAt(0) == '-' || expression.charAt(0) == '('
+					|| expression.contains("*")) {
+
 				final_term += term(expression);
 				final_expression_output += final_term;
 				
 				return final_expression_output;
-			}
-			else {
+			} else {
 				if (expression.charAt(i) == '+') {
 					
 					final_exp += term(expression.substring(0, i));
 					final_term += expression(expression.substring(i + 1));
-	
+
 					Pattern p = Pattern.compile(regex);
 					Matcher m_1 = p.matcher(final_exp);
 					Matcher m_2 = p.matcher(final_term);
-	
-					if ((m_1.find() && m_1.group().equals(final_exp)) 
+
+					if ((m_1.find() && m_1.group().equals(final_exp))
 							&& (m_2.find() && m_2.group().equals(final_term))) {
-	
+
 						e += Integer.parseInt(final_exp);
 						t += Integer.parseInt(final_term);
 						et_output += e + t;
 						final_expression_output += Integer.toString(et_output);
 						
 						return final_expression_output;
-					} 
-					else {
-						
+					} else {
 						final_expression_output += final_exp + final_term;
 						return final_expression_output;
 					}
-				} 
-				else if (expression.charAt(i) == '-') {
-					
-					for(int j = i+1; j < expression.length(); j++) {
+				} else if (expression.charAt(i) == '-') {
+
+					int last_plus = i;
+					for (int j = i + 1; j < expression.length(); j++) {				
 						
-						if(expression.charAt(j) == '-') {
+						if (expression.charAt(j) == '-') {
 							
-							String additional = "";
-							int r = 0;
-							final_exp += expression(expression.substring(0, i));
-							final_term += term(expression.substring(i+1,j));
-							additional += term(expression.substring(j+1));
-								
-							Pattern p = Pattern.compile(regex);
-							Matcher m_1 = p.matcher(final_exp);
-							Matcher m_2 = p.matcher(final_term);
-							Matcher m_3 = p.matcher(additional);
-			
-							if ((m_1.find() && m_1.group().equals(final_exp)) 
-									&& (m_2.find() && m_2.group().equals(final_term))
-									&& (m_3.find() && m_3.group().equals(additional))) {
-			
-								e += Integer.parseInt(final_exp);
-								t += Integer.parseInt(final_term);
-								r += Integer.parseInt(additional);
-								et_output += e - t - r;
-								
-								final_expression_output += Integer.toString(et_output);
-								return final_expression_output;
-							} 
-							else {
-								final_expression_output += final_exp + final_term + additional;
-								return final_expression_output;
-							}									
+							last_plus = j;
+							continue;
 						}
 					}
+					final_exp += expression(expression.substring(0, last_plus));
+					final_term += term(expression.substring(last_plus + 1));
 					
-					final_exp += expression(expression.substring(0, i));
-					final_term += term(expression.substring(i + 1));
-	
 					Pattern p = Pattern.compile(regex);
 					Matcher m_1 = p.matcher(final_exp);
 					Matcher m_2 = p.matcher(final_term);
-	
-					if ((m_1.find() && m_1.group().equals(final_exp)) 
+					
+					if ((m_1.find() && m_1.group().equals(final_exp))
 							&& (m_2.find() && m_2.group().equals(final_term))) {
-	
+
 						e += Integer.parseInt(final_exp);
 						t += Integer.parseInt(final_term);
+						
 						et_output += e - t;
 						final_expression_output += Integer.toString(et_output);
-						
 						return final_expression_output;
-					} 
-					else {
-						
+					} else {
 						final_expression_output += final_exp + final_term;
 						return final_expression_output;
 					}
@@ -280,39 +245,39 @@ public class Main {
 		String final_fact = "";
 		String final_term_output = "";
 		int e = 0, t = 0, et_output = 0;
-		String regex = "[+-]?[1-9]+";
+		String regex = "[+-]?[0-9]+";
+
+		// String term = Integer.toString(ter);
 
 		if (!term.contains("*")) {
-
+			
+			count = 0;
 			final_fact += fact(term);
 			final_term_output += final_fact;
 			
 			return final_term_output;
 		}
-		
+
 		for (int i = 0; i < term.length(); i++) {
-			
 			if (term.charAt(i) == '*') {
 
 				final_term += term(term.substring(0, i));
+				count = 0;
 				final_fact += fact(term.substring(i + 1));
-
+	
 				Pattern p = Pattern.compile(regex);
 				Matcher m_1 = p.matcher(final_term);
 				Matcher m_2 = p.matcher(final_fact);
 
-				if ((m_1.find() && m_1.group().equals(final_term)) 
-						&& (m_2.find() && m_2.group().equals(final_fact))) {
+				if ((m_1.find() && m_1.group().equals(final_term)) && (m_2.find() && m_2.group().equals(final_fact))) {
 
 					e += Integer.parseInt(final_term);
 					t += Integer.parseInt(final_fact);
 					et_output += e * t;
 					final_term_output += Integer.toString(et_output);
-
-					return final_term_output;
-				}
-				else {
 					
+					return final_term_output;
+				} else {
 					final_term_output += final_term + final_fact;
 					return final_term_output;
 				}
@@ -325,69 +290,64 @@ public class Main {
 
 		String final_fact_1 = "";
 		String final_fact_output = "";
-		
-		for(int p=0; p<fact.length(); p++) {
-			if(fact.charAt(p) == '-') {
+
+		for (int p = 0; p < fact.length(); p++) {
+			if (fact.charAt(p) == '-') {
 				count++;
-			}
-			else {
+			} else {
 				break;
 			}
 		}
 
-		if(fact.length() == 1) {
+		if (fact.length() == 1) {
 			count = 0;
 		}
 
-		if (!fact.contains("+") && !fact.contains("-") 
-				&& !fact.contains("(") && !fact.contains(")")) {
+		if (!fact.contains("+") && !fact.contains("-") && !fact.contains("(") && !fact.contains(")")) {
 
 			final_fact_1 = literal(fact);
-			
-			if(final_fact_1.equals("")) {
+
+			if (final_fact_1.equals("")) {
 				final_fact_1 = identifier(fact);
 			}
 		}
 
 		for (int i = 0; i < fact.length(); i++) {
-			
+
 			if (fact.charAt(0) == '(') {
 
 				int j;
 				for (j = i + 1; j < fact.length(); j++) {
-					
+
 					if (fact.charAt(j) == ')') {
-	
+
 						final_fact_1 += expression(fact.substring(i + 1, j));
 						final_fact_output += final_fact_1;
-						
+
 						return final_fact_output;
 					}
 				}
 				i = j;
-			}
-			else if (fact.charAt(0) == '-') {
-	
-				if(count % 2 == 1) {
-					final_fact_1 += "-" + fact(fact.substring(count));	
+			} else if (fact.charAt(0) == '-') {
+
+				if (count % 2 == 1) {
+					final_fact_1 += "-" + fact(fact.substring(count));
 					final_fact_output += final_fact_1;
-				}
-				else {
-					final_fact_1 += fact(fact.substring(count));	
+				} else {
+					final_fact_1 += fact(fact.substring(count));
 					final_fact_output += final_fact_1;
 				}
 
 				return final_fact_output;
-			}
-			else if (fact.charAt(0) == '+') {
-				
-				final_fact_1 += fact(fact.substring(i+1));	
+			} else if (fact.charAt(0) == '+') {
+
+				final_fact_1 += fact(fact.substring(i + 1));
 				final_fact_output += final_fact_1;
-			
+
 				return final_fact_output;
 			}
-		}	
-		
+		}
+
 		final_fact_output += final_fact_1;
 
 		return final_fact_output;
@@ -400,7 +360,7 @@ public class Main {
 			boolean output_1 = false, output_2 = false;
 			if (i == 0) {
 				output_1 = output_2 = letter(identifier.charAt(i));
-				
+	
 				if (output_1 == false && output_2 == false) {
 					return "";
 				}
@@ -421,12 +381,9 @@ public class Main {
 		String new_letter = String.valueOf(letter);
 
 		if (new_letter.matches("^[a-zA-Z]*$") || new_letter.equals("_")) {
-			
 			return true;
 		}
-		else {
-			return false;
-		}	
+		return false;
 	}
 
 	public static String literal(String literal) {
@@ -435,29 +392,24 @@ public class Main {
 
 			boolean output_1 = false;
 			if (i == 0) {
-				
 				if (literal.charAt(i) == '0') {
-					
-					if(literal.length() > 1) {
-						
-						if(literal.charAt(1) != '0') {
-							output_1 = true;
-						}
-						else {
+					if (literal.length() > 1) {
+						if (literal.charAt(1) != '0') {
+							// System.out.println("first zero");
+							output_1 = non_zero_digit(literal.charAt(1));
+						} else {
 							output_1 = false;
 							return "";
-						}			
-					}	
+						}
+					}
 					output_1 = true;
-				}
-				else {
+				} else {
 					output_1 = non_zero_digit(literal.charAt(i));
 				}
-			} 
-			else {
+			} else {
 				output_1 = digit(literal.charAt(i));
 			}
-			
+
 			if (output_1 == false) {
 				return "";
 			}
@@ -474,10 +426,9 @@ public class Main {
 		Matcher m = p.matcher(new_non_zero_digit);
 
 		if (m.find() && m.group().equals(new_non_zero_digit)) {
-
+		
 			return true;
-		} 
-		else {
+		} else {
 			return false;
 		}
 
@@ -492,10 +443,9 @@ public class Main {
 		Matcher m = p.matcher(new_digit);
 
 		if (m.find() && m.group().equals(new_digit)) {
-
+	
 			return true;
-		} 
-		else {
+		} else {
 			return false;
 		}
 	}
